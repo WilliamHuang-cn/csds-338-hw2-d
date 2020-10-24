fec59bff36c78a6792d0918cbb95c46234b4255f
William Huang <huang.william.cn@hotmail.com>
2020-10-24 18:25:53 -0400

    Modify Memory pages

--- memory.py
+++ memory.py
@@ -6,18 +6,38 @@ MEM_LEN = 3;
 class Memory:
     def __init__(self):
         self.physicalMemory = [];
+        self.virtualMemory = [];
         pass;
 
-    # Insert a page into memory
-    def insertPage():
+    # Allocate pages into physical Memory
+    def malloc(numberOfPages):
         pass;
 
-    # Search for a page
-    def searchPage():
+    # Swap designated pages in to memory
+    def swapIn(pageIndex):
+        pass;
+
+    # Swap designated pages out of memory
+    def swapOut(pageIndex):
+        pass;
+
+    # Read from a certain page
+    def access(pageIndex):
+        pass;
+
+    # Free a page from physical/virtual memory
+    def free(pageIndex):
         pass;
 
 class MemoryFIFO(memory):
-    pass;
+
+    # Override with FIFO algorithm
+    def swapOut(pageIndex):
+        pass;
 
 class MemoryCLOCK(memory):
+
+    # Override with CLOCK algorithm
+    def swapOut(pageIndex):
+        pass;
     pass;
--- page.py
+++ page.py
@@ -1,4 +1,6 @@
 class Page:
-    def __init__(self):
+    def __init__(self, index):
+        self.index = index;
         self.dirty = 0;
         self.referenced = 0;
+        self.age = 0;
