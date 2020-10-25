6678492a4677e5b700cca07507099d7e439bcc0c
woodman <dxl540@case.edu>
2020-10-25 00:31:46 -0400

    wrapout for clock

--- memory.py
+++ memory.py
@@ -7,6 +7,7 @@ class Memory:
     def __init__(self):
         self.physicalMemory = [None]*MEM_LEN;
         self.virtualMemory = [];        # We assume the size of virtual memory to be infinitive
+        self.clockHand = 0;
         pass;
 
     # Allocate pages in physical Memory
@@ -86,6 +87,34 @@ class Memory:
 
 class MemoryFIFO(Memory):
 
+    # Allocate pages in physical Memory
+    def alloc(self, pageList):
+        for page in pageList:
+            memIndex = self.nextFreeSpace();
+            if memIndex == -1: 
+                # Not enough memory avaliable. Page fault
+                # Need to swap one out
+                memIndex = self.swapOut();
+            self.physicalMemory[memIndex] = page;
+        for i in range(len(self.physicalMemory)):
+            self.physicalMemory[i].age = self.physicalMemory[i].age + 1
+
+
+    # Swap designated pages in to memory
+    def swapIn(self, pageIndex):
+        # Find page in virtual memtory
+        tmpIndex = self.findPage(self.virtualMemory, pageIndex);
+        if tmpIndex == -1: 
+            raise LookupError("Page not found in virtual memory.");
+        tmpPage = self.virtualMemory.pop(tmpIndex);
+        # Get a free position, or swap one out 
+        memIndex = self.nextFreeSpace();
+        if memIndex == -1:
+            memIndex = self.swapOut();
+        self.physicalMemory[memIndex] = tmpPage;
+        for i in range(len(self.physicalMemory)):
+            self.physicalMemory[i].age = self.physicalMemory[i].age + 1
+
     # Override with FIFO algorithm
     def swapOut(self):
         temp = 0
@@ -104,11 +133,19 @@ class MemoryFIFO(Memory):
 
 class MemoryCLOCK(Memory):
 
+   
     # Override with CLOCK algorithm
     def swapOut(self):
-        page =  self.physicalMemory.pop(clockHand)
-        self.virtualMemory.append(page)
-        self.physicalMemory.insert(clockHand, None)
+        flag = True
+        while(flag):
+            if self.physicalMemory[self.clockHand].referenced = 0:
+                page =  self.physicalMemory.pop(self.clockHand)
+                self.virtualMemory.append(page)
+                self.physicalMemory.insert(self.clockHand, None)
+                flag = False
+            else:
+                self.physicalMemory[self.clockHand].referenced = 0;
+        self.clockHand = self.clockHand + 1;
         return clockHand
             
         pass;
