4a63d1495c98002ea198d172a2ed3cb51fd448ed
William Huang <huang.william.cn@hotmail.com>
2020-10-25 00:54:41 -0400

    Add representation

--- page.py
+++ page.py
@@ -4,3 +4,9 @@ class Page:
         self.dirty = 0;
         self.referenced = referenced;
         self.age = 0;
+
+    def __str__(self):
+        return str(self.index);
+    
+    def __repr__(self):
+        return 'Page with index '+str(self.index);
